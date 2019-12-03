ce0d44436077e2b394b9b2ea9a96aa6f094531ea
Omar Sánchez Martínez <omar@posibilidades.com.mx>
2019-12-03 11:36:06 -0600

    Test de statuses creados

---
+++ app/Http/Controllers/StatusesController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\status;
+use Illuminate\Http\Request;
+
+class StatusesController extends Controller
+{
+    public function store()
+    {
+
+        request()->validate([
+            'body' => 'required|min:5'
+        ]);
+
+        $status = Status::create([
+            'body' => request('body'),
+            'user_id' => auth()->id(),
+        ]);
+
+        return response()->json([
+            'body' => $status->body
+        ]);
+    }
+}
---
+++ tests/Feature/CrateStatusTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CrateStatusTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     ** @test
+     **/
+    public function guest_users_can_not_create_statuses()
+    {
+
+        $response = $this->post(route('statuses.store'), ['body' => 'Mi primer status']);
+
+        $response->assertRedirect('login');
+    }
+
+    /**
+     * @test
+     */
+    public function an_authenticated_user_can_create_statuses()
+    {
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+
+        $response = $this->post(route('statuses.store'), ['body' => 'Mi primer status']);
+
+        $response->assertSuccessful();
+
+        $response->assertJson([
+            'body' => 'Mi primer status'
+        ]);
+
+    }
+
+    /**
+     ** @test
+     **/
+    public function a_status_requires_a_body()
+    {
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+
+        $response = $this->postJson(route('statuses.store'), ['body' => '']);
+
+        $response->assertStatus(422);
+
+        $response->assertJsonStructure([
+            'message', 'errors' => ['body']
+        ]);
+    }
+
+    /**
+     ** @test
+     **/
+    public function a_status_body_requires_a_minimum_length(){
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+
+        $response = $this->postJson(route('statuses.store'), ['body' => 'asd']);
+
+        $response->assertStatus(422);
+
+        $response->assertJsonStructure([
+            'message', 'errors' => ['body']
+        ]);
+    }
+}
